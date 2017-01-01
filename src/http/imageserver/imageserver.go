84eb723b16fb598a896a94e76c07acb7c76a7a7d
Iuri Sitinschi <yukas66@gmail.com>
2017-01-01 22:15:12 +0100

    status handler

--- src/http/imageserver/imageserver.go
+++ src/http/imageserver/imageserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
     "net/http"
 	"regexp"
 	"os"
@@ -19,12 +20,32 @@ import (
 	"image/jpeg"
 )
 
+var queryCount int = 0
+var failedQueryCount int = 0
+var startTime time.Time = time.Now()
+
+func statusHandler(w http.ResponseWriter, r *http.Request, filename string) {
+	fmt.Fprintln(w, "Status:\n")
+	fmt.Fprintf(w, "Query count: %v\n", queryCount)
+	fmt.Fprintf(w, "Failed query count: %v\n", failedQueryCount)
+	fmt.Fprintf(w, "Start time: %v\n", startTime)
+	fmt.Fprintf(w, "Running time: %v\n", time.Since(startTime))
+	
+	
+	debug.FreeOSMemory()
+}
+
 var imgCache *cache.Cache
 
 func imageHandler(w http.ResponseWriter, r *http.Request, filename string) {
 	defer timeTrack(time.Now(), filename)
 	
 	image := getImageByName(filename)
+	if (image == nil) {
+		failedQueryCount += 1
+		http.NotFound(w, r)			
+        return
+	}
 	
 	width,_ := strconv.Atoi(r.URL.Query().Get("w"))
 	height,_ := strconv.Atoi(r.URL.Query().Get("h"))
@@ -36,12 +57,16 @@ func imageHandler(w http.ResponseWriter, r *http.Request, filename string) {
 	writeImage(w, image);
 	
 	debug.FreeOSMemory()
+	queryCount += 1
 }
 
 func getImageByName(filename string) *image.Image {
 	image := imgCache.Get(filename)
 	if (image == nil) {
 		image = reader.Decode(filename)
+		if (image == nil) {			
+			return nil
+		}
 		imgCache.Set(filename, image)
 	}
 	
@@ -53,22 +78,25 @@ func writeImage(w http.ResponseWriter, img *image.Image) {
     buffer := new(bytes.Buffer)
     if err := jpeg.Encode(buffer, *img, nil); err != nil {
         log.Println("unable to encode image.")
+		failedQueryCount += 1
     }
 
     w.Header().Set("Content-Type", "image/jpeg")
     w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
     if _, err := w.Write(buffer.Bytes()); err != nil {
         log.Println("unable to write image.")
+		failedQueryCount += 1
     }
 }
 
-var validPath = regexp.MustCompile("^/(image)/(.*)$")
+var validPath = regexp.MustCompile("^/(image|status)/(.*)$")
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
         if m == nil {
-            http.NotFound(w, r)
+            failedQueryCount += 1
+			http.NotFound(w, r)			
             return
         }
         fn(w, r, m[2])
@@ -101,6 +129,7 @@ func startServer() {
 
 func listenAndServe(port string) {
 	http.HandleFunc("/image/", makeHandler(imageHandler))
+	http.HandleFunc("/status/", makeHandler(statusHandler))
     http.ListenAndServe(":" + port, nil)
 }
 
